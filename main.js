d188f0ab15847d7c1f6371f4f1bf3b36d2fff2f3
rakib <sohrabhosen16105@gmail.com>
2023-07-28 20:25:07 +0600

    navbar img changed

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-var body=document.getElementsByTagName('body')[0];
-console.log(body);
-body.addEventListener('scroll',(()=>{
-    alert('hi')
-}))
+let api=document.getElementById('fetch');
+console.log(api)
+
+const datas=fetch("http://localhost:8081/users")
+.then((res)=>{
+    return res.json()
+})
+.then((datas)=>{
+    console.log(datas.users[0].name);
+    
+    datas.users.map((user)=>{
+        const {id,name}=user;
+        api.innerHTML=`
+        <p> ${id}</p>
+        <h2>${name}</h2> `
+    })
+});
+  
